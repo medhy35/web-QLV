6e3487ef284a90843e558bcab81fb2e0a27a94ee
Medhy <mahamadoun.toure1@gmail.com>
2026-05-20 10:37:26 +0200

    feat: scaffold Next.js 15 project with all dependencies

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from 'next'
+import createNextIntlPlugin from 'next-intl/plugin'
+
+const withNextIntl = createNextIntlPlugin('./i18n/request.ts')
+
+const config: NextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'cdn.sanity.io' },
+      { protocol: 'https', hostname: 'www.instagram.com' },
+    ],
+  },
+}
+
+export default withNextIntl(config)
